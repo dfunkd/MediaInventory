8dff92be6ae0a3d3dd225c2aeac3bc3c2b8f3439
Dylan Funk <Dylan@ortho2.com>
2016-08-25 15:43:14 -0500

    Created skeleton for Add Recipe Control

---
+++ MediaInventory/MediaInventory/Data/RecipeIngredient.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+
+namespace MediaInventory.Data
+{
+    public class RecipeIngredient : INotifyPropertyChanged
+    {
+        #region INotivyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+        #region Private Properties
+        private decimal _quantity = 0;
+        private Measurement _measurement;
+        private Ingredient _ingredient;
+        #endregion
+        public RecipeIngredient(decimal quantity, Measurement measurement, Ingredient ingredient)
+        {
+            _quantity = quantity;
+            _measurement = measurement;
+            _ingredient = ingredient;
+        }
+        #region Public Properties
+        public decimal Quantity
+        {
+            get { return _quantity; }
+            private set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    if (PropertyChanged != null)
+                        RaisePropertyChanged("Quantity");
+                }
+            }
+        }
+        public Measurement Measurement
+        {
+            get { return _measurement; }
+            private set
+            {
+                if (_measurement != value)
+                {
+                    _measurement = value;
+                    if (PropertyChanged != null)
+                        RaisePropertyChanged("Measurement");
+                }
+            }
+        }
+        public Ingredient Ingredient
+        {
+            get { return _ingredient; }
+            private set
+            {
+                if (_ingredient != value)
+                {
+                    _ingredient = value;
+                    if (PropertyChanged != null)
+                        RaisePropertyChanged("Ingredient");
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ MediaInventory/MediaInventory/UserControls/AddRecipe.xaml.cs
@@ -0,0 +1,94 @@
+﻿using MediaInventory.Data;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MediaInventory.UserControls
+{
+    public partial class AddRecipe : UserControl
+    {
+        #region Dependency Properties
+        #region RecipeIngredients
+        public static readonly DependencyProperty RecipeIngredientsProperty = DependencyProperty.Register("RecipeIngredients", typeof(List<Ingredient>), typeof(AddRecipe), new FrameworkPropertyMetadata(new List<Ingredient>(),
+            FrameworkPropertyMetadataOptions.None, new PropertyChangedCallback(OnRecipeIngredientsChanged)));
+        public List<Ingredient> RecipeIngredients
+        {
+            get { return (List<Ingredient>)GetValue(RecipeIngredientsProperty); }
+            set { SetValue(RecipeIngredientsProperty, value); }
+        }
+        private static void OnRecipeIngredientsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((AddRecipe)d).OnRecipeIngredientsChanged(e);
+        }
+        protected virtual void OnRecipeIngredientsChanged(DependencyPropertyChangedEventArgs e)
+        {
+        }
+        #endregion
+        #region RecipeImage
+        public static readonly DependencyProperty RecipeImageProperty = DependencyProperty.Register("RecipeImage", typeof(BitmapImage), typeof(AddRecipe), new FrameworkPropertyMetadata(new BitmapImage()));
+        public BitmapImage RecipeImage
+        {
+            get { return (BitmapImage)GetValue(RecipeImageProperty); }
+            set { SetValue(RecipeImageProperty, value); }
+        }
+        #endregion
+        #endregion
+        public AddRecipe()
+        {
+            InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                ChangeRecipeImage(@"/MediaInventory;component/Resources/Images/no_photo.jpg");
+                imgRecipe.Source = RecipeImage;
+            };
+            txtSearchCriteria.txtContent.TextChanged += (s, e) =>
+            {
+                var searchCriteria = e.Source.ToString();
+                if (DataHelper.Default.Entity.Recipes.Where(w => w.Name == searchCriteria).FirstOrDefault() == null)
+                    txtSearchCriteria.Background = ((Brush)(new BrushConverter()).ConvertFromString("#00FF0000"));
+                else
+                    txtSearchCriteria.Background = ((Brush)(new BrushConverter()).ConvertFromString("#FFFF0000"));
+            };
+        }
+        #region Events
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".png";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            bool? result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                ChangeRecipeImage(filename);
+                imgRecipe.Source = RecipeImage;
+            }
+        }
+        #endregion
+        #region Private Methods
+        private void ChangeRecipeImage(string fileLocation)
+        {
+            RecipeImage = new BitmapImage();
+            RecipeImage.BeginInit();
+            RecipeImage.UriSource = new Uri(fileLocation, UriKind.RelativeOrAbsolute);
+            RecipeImage.EndInit();
+        }
+        #endregion
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            int prepTime, cookTime;
+            if (!int.TryParse(txtPrepTime.Text, out prepTime))
+                prepTime = 0;
+            if (!int.TryParse(txtCookTime.Text, out cookTime))
+                cookTime = 0;
+            var totalTime = prepTime + cookTime;
+            var ts = TimeSpan.FromMinutes(totalTime);
+            tbTotalTime.Text = string.Format("{0}:{1}", ts.Hours, ts.Minutes);
+        }
+    }
+}
