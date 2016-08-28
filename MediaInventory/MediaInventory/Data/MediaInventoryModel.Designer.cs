4dfd9c07b972ec660d4db4a59409af1768c96665
Dylan (Home) <dylanpfunk4@yahoo.com>
2016-08-28 13:12:42 -0500

    Updated Entity Framework to include Servings.

--- MediaInventory/MediaInventory/Data/MediaInventoryModel.Designer.cs
+++ MediaInventory/MediaInventory/Data/MediaInventoryModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\GitRepo\MediaInventory\MediaInventory\Data\MediaInventoryModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GitRepo\MediaInventory\MediaInventory\MediaInventory\Data\MediaInventoryModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- MediaInventory/MediaInventory/Data/Recipe.cs
+++ MediaInventory/MediaInventory/Data/Recipe.cs
@@ -26,9 +26,10 @@ namespace MediaInventory.Data
         public byte[] Image { get; set; }
         public string Directions { get; set; }
         public Nullable<bool> Valuation { get; set; }
-        public string recRecipeAcquiredFrom { get; set; }
+        public string RecipeAcquiredFrom { get; set; }
         public Nullable<int> PrepTimeMinutes { get; set; }
         public Nullable<int> CookTimeMinutes { get; set; }
+        public string Servings { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Recipe2Ingredient2Measurement> Recipe2Ingredient2Measurement { get; set; }
